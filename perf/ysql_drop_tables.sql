dccc920495c3d287d748348eccca88051e3e8003
Rajat Venkatesh <1638298+vrajat@users.noreply.github.com>
2022-07-14 22:40:13 +0530

    Performance Framework for CDCSDK
    The framework provides scripts and instructions to:
    - Setup a dev server with required dependencies
    - Docker compose files to run CDCSDK Server, Prometheus and Grafana
    - Organize yaml files for composition.
    - Scripts to compile and run workloads.
    - Support Null and Kafka as sinks
    
    A README with all instructions.

---
+++ perf/ysql_drop_tables.sql
@@ -0,0 +1,11 @@
+DROP TABLE IF EXISTS  api_sub_apple_orig_transactions;
+DROP table IF EXISTS api_apple_store_receipt_log; 
+DROP table IF EXISTS api_oauth_access_token;
+DROP table IF EXISTS api_sub_recurly_notify_log; 
+DROP table IF EXISTS api_sub_user_packages;
+DROP table IF EXISTS api_user_device; 
+DROP table IF EXISTS api_user_partner_transaction_log;
+DROP table IF EXISTS api_watch_list; 
+DROP table IF EXISTS user_profile;
+
+
