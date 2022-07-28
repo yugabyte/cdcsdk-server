5223fdeb53926939dc997a4921e02fb8268f0706
Vaibhav Kushwaha <vkushwaha@yugabyte.com>
2022-07-28 17:17:05 +0000

    used auto formatter

--- cdcsdk-testing/src/test/java/com/yugabyte/cdcsdk/testing/util/TestImages.java
+++ cdcsdk-testing/src/test/java/com/yugabyte/cdcsdk/testing/util/TestImages.java
@@ -7,18 +7,18 @@ import org.testcontainers.utility.DockerImageName;
  * @author Vaibhav Kushwaha (vkushwaha@yugabyte.com)
  */
 public class TestImages {
-  public static final DockerImageName KAFKA = DockerImageName.parse("confluentinc/cp-kafka:6.2.1");
+    public static final DockerImageName KAFKA = DockerImageName.parse("confluentinc/cp-kafka:6.2.1");
 
-  public static final DockerImageName POSTGRES = DockerImageName.parse("debezium/example-postgres:1.6").asCompatibleSubstituteFor("postgres");
+    public static final DockerImageName POSTGRES = DockerImageName.parse("debezium/example-postgres:1.6").asCompatibleSubstituteFor("postgres");
 
-  public static final DockerImageName CDCSDK_SERVER = DockerImageName.parse("quay.io/yugabyte/cdcsdk-server:latest");
+    public static final DockerImageName CDCSDK_SERVER = DockerImageName.parse("quay.io/yugabyte/cdcsdk-server:latest");
 
-  public static final String ELASTICSEARCH_IMG_NAME = "docker.elastic.co/elasticsearch/elasticsearch:7.3.0";
+    public static final String ELASTICSEARCH_IMG_NAME = "docker.elastic.co/elasticsearch/elasticsearch:7.3.0";
 
-  // This image contains the Kafka Connect image along with the required Elasticsearch sink connector
-  public static final String KAFKA_CONNECT_ES = "quay.io/yugabyte/connect-jdbc-es:1.0";
+    // This image contains the Kafka Connect image along with the required Elasticsearch sink connector
+    public static final String KAFKA_CONNECT_ES = "quay.io/yugabyte/connect-jdbc-es:1.0";
 
-  // This Kafka Connect image contains the required drivers and connectors
-  // i.e. Postgres JDBC driver, MySql JDBC driver, JDBCSinkConnector
-  public static final String KAFKA_CONNECT = "quay.io/yugabyte/debezium-connector:1.3.7-BETA";
+    // This Kafka Connect image contains the required drivers and connectors
+    // i.e. Postgres JDBC driver, MySql JDBC driver, JDBCSinkConnector
+    public static final String KAFKA_CONNECT = "quay.io/yugabyte/debezium-connector:1.3.7-BETA";
 }
