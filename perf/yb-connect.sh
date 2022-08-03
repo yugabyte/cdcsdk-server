0f794391b85e57b6e91640151d515bc9bfb2958e
Rajat Venkatesh <1638298+vrajat@users.noreply.github.com>
2022-08-03 23:05:09 +0530

    Dockerfile and shell script to run Kafka Connect

---
+++ perf/yb-connect.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+set -xe
+FILE=$1
+
+if test -f "$FILE"; then
+    export $(grep -v '^#' $FILE | xargs)
+fi
+
+export PATH=$PATH:$YBPATH
+
+payload() {
+    cat << EOF
+{
+    "name": "yb-connector-${TOPIC_PREFIX}-${FQDN[0]}-${FQDN[1]}",
+    "config": {
+        "connector.class":"io.debezium.connector.yugabytedb.YugabyteDBConnector",    
+        "tasks.max": "${THREADS}",
+        "topics": "${TOPIC_PREFIX}.${table}",
+        "database.hostname":"${PGHOST}",
+        "database.port":"5433",
+        "database.master.addresses": "${PGHOST}:7100",
+        "database.user": "yugabyte",
+        "database.password": "yugabyte",
+        "database.dbname" : "yugabyte",
+        "database.server.name": "${TOPIC_PREFIX}",
+        "table.include.list":"${TABLES}",
+        "database.streamid":"${CDC_SDK_STREAM_ID}",
+        "snapshot.mode":"never"
+    }
+}
+EOF
+}
+
+IFS=',' read -ra TABLE_ARRAY <<< "$TABLES"
+for table in "${TABLE_ARRAY[@]}"; do
+    echo $table
+    IFS='.' read -ra FQDN <<< "$table"
+    curl -i -X POST -H "Accept:application/json" -H "Content-Type:application/json" localhost:8083/connectors/ -d "$(payload)"
+
+
+done
